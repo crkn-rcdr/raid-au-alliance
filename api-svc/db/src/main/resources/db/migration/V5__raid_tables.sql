801ac7fdbc284a22af941fa5789b1fd9f3eb78d2
mary-crkn <mzheng@crkn.ca>
2026-07-15 21:27:19 -0400

    added api_svc schema to v4_sign_in_tables.sql, v5+_raid_tables.sql and v6_import_ervice_points.sql

--- api-svc/db/src/main/resources/db/migration/V5__raid_tables.sql
+++ api-svc/db/src/main/resources/db/migration/V5__raid_tables.sql
@@ -1,13 +1,13 @@
-drop table if exists raid cascade;
+drop table if exists api_svc.raid cascade;
 drop table if exists raid_v2 cascade;
 
 drop type if exists metaschema;
 
 create type metaschema as enum ('raido-metadata-schema-v1', 'legacy-metadata-schema-v1');
 
-create table raid (
+create table api_svc.raid (
   handle           varchar(32) primary key                                     not null,
-  service_point_id bigint references service_point                             not null,
+  service_point_id bigint references api_svc.service_point                             not null,
   url              varchar(512)                                                not null,
   url_index        integer                                                     not null,
   primary_title    varchar(256)                                                not null,
@@ -18,16 +18,16 @@ create table raid (
   date_created     timestamp without time zone default transaction_timestamp() not null
 );
 
-comment on column raid.url is
+comment on column api_svc.raid.url is
   'The value that we set as the `URL` property via ARDC APIDS.
   Example: `https://demo.raido-infra.com/raid/123.456/789`. 
   The global handle regisrty url (e.g. `https://hdl.handle.net/123.456/789`) 
   will redirect to this value.';
 
-comment on column raid.url_index is
+comment on column api_svc.raid.url_index is
   'The `index` of the URL property in APIDS. This can be different if we change
   how we mint URL values via APIDS.';
 
-comment on column raid.metadata_schema is
+comment on column api_svc.raid.metadata_schema is
   'Identifies the structure of the data in the metadata column';
 
